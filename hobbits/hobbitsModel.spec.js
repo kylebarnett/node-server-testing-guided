dfaaac6e6b92be7a90d04ddfbc40016b4c231943
Kyle Barnett <kbarnett23@gmail.com>
2019-12-11 21:33:27 -0700

    finished guided

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -1,3 +1,25 @@
-test('true', () => {
-  expect(true).toBe(true)
+const Hobbits = require('./hobbitsModel.js');
+const db = require('../data/dbConfig.js');
+
+describe('hobbits model', () => {
+  describe('insert', () => {
+    it('should insert the hobbit into the db', async () => {
+      await Hobbits.insert({ name: 'gaffer' })
+      await Hobbits.insert({ name: 'sam' })
+      const hobbits = await db('hobbits')
+      expect(hobbits).toHaveLength(2);
+    })
+
+    it('should return the hobbit inserted', async () => {
+      let hobbit1 = await Hobbits.insert({ name: 'gaffer' });
+      expect(hobbit1.name).toBe('gaffer');
+
+      let hobbit2 = await Hobbits.insert({ name: 'sam' });
+      expect(hobbit2.name).toBe('sam');
+    });
+
+    beforeEach(async () => {
+      await db('hobbits').truncate()
+    })
+  })
 })
